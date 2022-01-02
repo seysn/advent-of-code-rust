6f50ca321249eb699bf95d185cd02342854c294d
Nicolas SEYS <seys.nicolas@gmail.com>
2022-01-02 23:08:31 +0100

    Add day 4 of year 2015

---
+++ src/y2015/d04.rs
@@ -0,0 +1,35 @@
+use md5;
+
+pub fn parse_input(input: &str) -> String {
+    String::from(input)
+}
+
+fn md5_starts_with(input: &str, pat: &str) -> u32 {
+    for i in u32::MIN..u32::MAX {
+        let digest = format!("{}{}", input, i);
+        let result = format!("{:x}", md5::compute(digest.as_bytes()));
+        if result.starts_with(pat) {
+            return i;
+        }
+    }
+    unreachable!();
+}
+
+pub fn part1(input: &str) -> u32 {
+    md5_starts_with(input, "00000")
+}
+
+pub fn part2(input: &str) -> u32 {
+    md5_starts_with(input, "000000")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_part1() {
+        assert_eq!(part1(&parse_input("abcdef")), 609043);
+        assert_eq!(part1(&parse_input("pqrstuv")), 1048970);
+    }
+}
