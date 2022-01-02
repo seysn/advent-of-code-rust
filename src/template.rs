e338245e22b894be32b4eec415e9092dd96ec3d9
Nicolas SEYS <seys.nicolas@gmail.com>
2022-01-02 15:40:37 +0100

    Add day template

---
+++ src/template.rs
@@ -0,0 +1,28 @@
+pub fn parse_input(input: &str) -> Vec<i32> {
+    vec![0]
+}
+
+pub fn part1(input: &[i32]) -> i32 {
+    0
+}
+
+pub fn part2(input: &[i32]) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &'static str = "";
+
+    #[test]
+    fn example_part1() {
+        assert_eq!(part1(&parse_input(EXAMPLE)), 0);
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(part2(&parse_input(EXAMPLE)), 0);
+    }
+}
