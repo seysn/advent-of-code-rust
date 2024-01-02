ef7e7503fc0cabddd52057410f1df59502dcadd5
Nicolas SEYS <seys.nicolas@gmail.com>
2024-01-02 01:23:33 +0100

    Optimize day 5 of year 2023

--- src/y2023/d05.rs
+++ src/y2023/d05.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 struct ConvertLine {
 	destination_range_start: u64,
 	source_range_start: u64,
@@ -21,6 +23,20 @@ impl ConvertLine {
 	fn convert(&self, value: u64) -> u64 {
 		value - self.source_range_start + self.destination_range_start
 	}
+
+	fn sub_range(&self, range: &Range<u64>) -> Option<Range<u64>> {
+		let src_start = self.source_range_start;
+		let src_end = self.source_range_start + self.range_length;
+
+		let sub_start = src_start.max(range.start);
+		let sub_end = src_end.min(range.end);
+
+		if sub_start < sub_end {
+			Some(sub_start..sub_end)
+		} else {
+			None
+		}
+	}
 }
 
 impl Map {
@@ -32,6 +48,29 @@ impl Map {
 		}
 
 		value
+	}
+
+	fn convert_range(&self, range: Range<u64>) -> Vec<Range<u64>> {
+		let mut res = Vec::new();
+		let mut ranges = vec![range];
+
+		'outer: while let Some(range) = ranges.pop() {
+			for convert_line in &self.convert_lines {
+				if let Some(sub_range) = convert_line.sub_range(&range) {
+					res.push(convert_line.convert(sub_range.start)..convert_line.convert(sub_range.end));
+					if sub_range.start > range.start {
+						ranges.push(range.start..sub_range.start);
+					}
+					if sub_range.end < range.end {
+						ranges.push(sub_range.end..range.end);
+					}
+					continue 'outer;
+				}
+			}
+			res.push(range);
+		}
+
+		res
 	}
 }
 
@@ -87,30 +126,24 @@ pub fn part1(input: &Almanac) -> u64 {
 	location
 }
 
-#[allow(unused)]
 pub fn part2(input: &Almanac) -> u64 {
-	// TODO: Optimize
+	let mut ranges = Vec::new();
+	for seed_pair in input.seeds.chunks(2) {
+		ranges.push(seed_pair[0]..seed_pair[0] + seed_pair[1]);
+	}
 
-	// let mut location = u64::MAX;
-	// for seed_pair in input.seeds.chunks(2) {
-	// 	let seed_start = seed_pair[0];
-	// 	let seed_end = seed_start + seed_pair[1];
-	// 	for seed in seed_start..seed_end {
-	// 		let mut value = seed;
+	for map in &input.maps {
+		let mut new_ranges = Vec::new();
 
-	// 		for map in &input.maps {
-	// 			value = map.convert(value);
-	// 		}
+		for range in ranges {
+			// Could be optimized by merging ranges but this is good enough
+			new_ranges.extend(map.convert_range(range));
+		}
 
-	// 		if value < location {
-	// 			location = value;
-	// 		}
-	// 	}
-	// }
+		ranges = new_ranges;
+	}
 
-	// location
-
-	46
+	ranges.iter().map(|range| range.start).min().unwrap()
 }
 
 #[cfg(test)]
